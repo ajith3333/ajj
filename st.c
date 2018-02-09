a8d82388d602b69d6e1f50b09fa41de4117ceff8
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-02-09 15:54:50 +0530

    st.c

---
+++ st.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char str[100];
+   int i = 0, l = 0, f = 1;
+   clrscr();
+   puts("Enter any string\n");
+   gets(str);
+   for(i = 0; str[i] !='\0'; i++)
+  
+   printf("The number of characters in the string are %d\n", l);
+   for(i = 0; i <= l-1; i++)
+   {
+      if(str[i] == ' ')
+   }
+   printf("The number of words in the string are %d", f);
+   getch();
+}
