9664c002886c3ca71dcb59fd52c6a044839db28c
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-02-09 15:43:53 +0530

    sp.c

---
+++ sp.c
@@ -0,0 +1,20 @@
+   #include<stdio.h>
+    int main(){
+                  char words[200];
+                  int  spaces=0;
+                  int flag=0, i;
+                  clrscr();
+                  printf("Enter a line of Text :\n");
+                 {
+                 if( || words[i]=='O' || words[i]=='U')
+                 ++vowels;
+                 else
+                 if((words[i]>='a'&& words[i]<='z') || (words[i]>='A'&& words[i]<='Z'))
+                 ++letters;
+                 else
+               
+                 printf("\n Number of White spaces:  %d", spaces);
+                 printf("\n Number of Characters:  %d", i);
+                 getch();
+    return 0;
+    }
